95cc648efc90101781244aae57b1614f9c096a34
rollingCheseWheel <rollingCheseWheel@DESKTOP-GQE8LCM>
2025-10-14 15:16:07 +0200

    tweaked and refactored IntToBoolConverter

--- Util/Converters.cs
+++ Util/Converters.cs
@@ -6,31 +6,19 @@ public class IntToBoolConverter : JsonConverter<bool>
 {
 	public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 	{
-		if (reader.TokenType == JsonTokenType.Number)
+		return reader.TokenType switch
 		{
-			int value = reader.GetInt32();
-			return value != 0;
-		}
-
-		if (reader.TokenType == JsonTokenType.True)
-		{
-			return true;
-		}
-		if (reader.TokenType == JsonTokenType.False)
-		{
-			return false;
-		}
-		if (reader.TokenType == JsonTokenType.Null)
-		{
-			return false;
-		}
-
-		throw new JsonException($"Unexpected token {reader.TokenType} when parsing bool.");
+			JsonTokenType.Number => reader.GetInt32() != 0,
+			JsonTokenType.True => true,
+			JsonTokenType.False => false,
+			JsonTokenType.Null => false,
+			_ => throw new JsonException($"Unexpected token {reader.TokenType} when parsing bool.")
+		};
 	}
 
 	public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
 	{
-		writer.WriteNumberValue(value ? 1 : 0);
+		writer.WriteBooleanValue(value);
 	}
 }
 
