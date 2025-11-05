4350a9f4732aaf3ff6a40592d7289390ae07bd36
rollingCheseWheel <rollingCheseWheel@DESKTOP-GQE8LCM>
2025-11-05 10:52:47 +0100

    slight refactor

---
+++ RegisterClient/RegisterPathEnum.cs
@@ -0,0 +1,20 @@
+﻿using Util;
+
+namespace registerClient;
+public sealed class RegisterPath(string value) : StringEnum(value)
+{
+	public static readonly RegisterPath Api = new("v2/api");
+
+	public static readonly RegisterPath LoginPage = new("login");
+
+	public static readonly RegisterPath Login = new("auth/login");
+	public static readonly RegisterPath Calendar = new("calendar/student");
+	public static readonly RegisterPath ProfileDetails = new("profile/get");
+
+	public override string ToString()
+	{
+		return base.ToString().EndsWith('/')
+			? throw new Exception("Paths cannot end in slashes")
+			: base.ToString();
+	}
+}
--- Util/Util.cs
+++ Util/Util.cs
@@ -16,24 +16,6 @@ public static class Constants
 	public const string StudentIdClaimName = "studentId";
 }
 
-public sealed class RegisterPath(string value) : StringEnum(value)
-{
-	public static readonly RegisterPath Api = new("v2/api");
-
-	public static readonly RegisterPath LoginPage = new("login");
-
-	public static readonly RegisterPath Login = new("auth/login");
-	public static readonly RegisterPath Calendar = new("calendar/student");
-	public static readonly RegisterPath ProfileDetails = new("profile/get");
-
-	public override string ToString()
-	{
-		return base.ToString().EndsWith('/')
-			? throw new Exception("Paths cannot end in slashes")
-			: base.ToString();
-	}
-}
-
 public class StringEnum
 {
 	public readonly string Value;
