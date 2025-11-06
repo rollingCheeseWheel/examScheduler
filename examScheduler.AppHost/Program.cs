e268d0d3d9bebd65aac5d500b139757944abe464
rollingCheeseWheel <manuelsinner.ms@gmail.com>
2025-11-06 22:38:06 +0100

    container now actually stores data persistently

--- examScheduler.AppHost/Program.cs
+++ examScheduler.AppHost/Program.cs
@@ -2,6 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("database") // container name
 	.WithLifetime(ContainerLifetime.Persistent)
+	.WithDataVolume()
 	.WithHostPort(5432)
 	.AddDatabase("postgres"); // connection string name
 
