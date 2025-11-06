6b08ced7e85650f71a009a832488a4b42bec90a8
rollingCheseWheel <rollingCheseWheel@DESKTOP-GQE8LCM>
2025-11-06 09:01:39 +0100

    added a new migration to account for the registerId removal on UserProfile

---
+++ examScheduler/Migrations/20251106080118_postgresql.container_migration_744.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace examScheduler.Migrations
+{
+    /// <inheritdoc />
+    public partial class postgresqlcontainer_migration_744 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserProfiles_RegisterUsername_SchoolId_RegisterId",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "RegisterId",
+                table: "UserProfiles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_RegisterUsername_SchoolId",
+                table: "UserProfiles",
+                columns: new[] { "RegisterUsername", "SchoolId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserProfiles_RegisterUsername_SchoolId",
+                table: "UserProfiles");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RegisterId",
+                table: "UserProfiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_RegisterUsername_SchoolId_RegisterId",
+                table: "UserProfiles",
+                columns: new[] { "RegisterUsername", "SchoolId", "RegisterId" },
+                unique: true);
+        }
+    }
+}
