df5ca0c525cb1d7963bbfd7b35c515e2658fc16c
rollingCheeseWheel <rollingcheesewheel1@gmail.com>
2026-01-30 00:33:03 +0100

    slightly changed dto

--- Models/API/SwapRequest.cs
+++ Models/API/SwapRequest.cs
@@ -7,8 +7,6 @@ public class SwapRequest
 	[Required]
 	public required Guid Id { get; set; }
 	[Required]
-	public required Guid ScheduleId { get; set; }
-	[Required]
 	public required string RequestingStudentName { get; set; }
 	[Required]
 	public required Guid RequestingStudentId { get; set; }
--- examScheduler/Mappings/SwapRequestMappings.cs
+++ examScheduler/Mappings/SwapRequestMappings.cs
@@ -7,9 +7,8 @@ public static class SwapRequestMappings
 	public static SwapRequest ToDTO(this Entities.SwapRequest entity) => new()
 	{
 		Id = entity.Id,
-		ScheduleId = entity.ScheduleId,
 		RequestingStudentName = entity.RequestingStudentName,
 		RequestingStudentId = entity.RequestingStudentId,
 		RequestedSlotId = entity.RequestedSlotId,
 	};
-}
+}	
